7c8dfd1cc05a65fa44eb3819a2184993d4777ce6
ndipa <n.diplma@web.de>
2024-10-18 15:46:01 +0200

    Added pfeiltasten and backspace with minor issue in stylingy

--- flipflOS/Kernel.cs
+++ flipflOS/Kernel.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.Design;
-using System.Text;
 using Sys = Cosmos.System;
 
 namespace flipflOS
@@ -15,52 +13,100 @@ namespace flipflOS
         {
             Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
             start = DateTime.Now;
-            
+
         }
 
         protected override void Run()
         {
-            Console.Write("Input: ");
-            var input = Console.ReadLine();
+            List<string> commandHistory = new List<string>();
+            int historyIndex = -1;
+            string currentInput = "";
 
-            Console.WriteLine(input);
-            String[] args = input.Split(' ');
-            switch (args[0])
+            while (true)
             {
-                case "help":
+                Console.Write("Input: ");
+                currentInput = "";
+
+                while (true)
+                {
+                    var key = Console.ReadKey(intercept: true);
+
+                    if (key.Key == ConsoleKey.Enter)
                     {
-                        help(args);
+                        Console.WriteLine();
+                        if (!string.IsNullOrWhiteSpace(currentInput))
+                        {
+                            commandHistory.Add(currentInput);
+                            historyIndex = commandHistory.Count; // Index auf das Ende setzen
+                        }
+                        break;
                     }
-                    break;
-                case "time":
+                    else if (key.Key == ConsoleKey.Backspace)
                     {
+                        if (currentInput.Length > 0)
+                        {
+                            currentInput = currentInput.Substring(0,currentInput.Length - 1); // Letztes Zeichen entfernen
+                            Console.Write("\r" + new string(' ', Console.WindowWidth) + "\rInput: " + currentInput);
+                        }
+                    }
+                    else if (key.Key == ConsoleKey.UpArrow)
+                    {
+                        if (historyIndex > 0)
+                        {
+                            historyIndex--;
+                            currentInput = commandHistory[historyIndex]; // Befehl aus der Historie holen
+                            Console.Write("\r" + new string(' ', Console.WindowWidth) + "\rInput: " + currentInput); // Befehl anzeigen
+                        }
+                    }
+                    else if (key.Key == ConsoleKey.DownArrow)
+                    {
+                        if (historyIndex < commandHistory.Count - 1)
+                        {
+                            historyIndex++;
+                            currentInput = commandHistory[historyIndex]; // Nächster Befehl aus der Historie
+                            Console.Write("\r" + new string(' ', Console.WindowWidth) + "\rInput: " + currentInput); // Befehl anzeigen
+                        }
+                        else
+                        {
+                            currentInput = ""; // Kein Befehl mehr, Eingabe leeren
+                            Console.Write("\r" + new string(' ', Console.WindowWidth) + "\rInput: ");
+                        }
+                    }
+                    else
+                    {
+                        currentInput += key.KeyChar;
+                        Console.Write(key.KeyChar);
+                    }
+                }
+
+                string[] args = currentInput.Split(' ');
+                switch (args[0])
+                {
+                    case "help":
+                        help(args);
+                        break;
+                    case "time":
                         TimeSpan runtime = DateTime.Now - start;
                         Console.WriteLine("running for: " + runtime.TotalSeconds + " seconds");
-                    }
-                    break;
-                case "write":
-                    {
+                        break;
+                    case "write":
                         writeToMemory(args);
-                    }
-                    break;
-                case "read":
-                    {
+                        break;
+                    case "read":
                         Console.WriteLine(readFromMemory(args));
-                    }
-                    break;
-                default:
-                    {
+                        break;
+                    default:
                         Console.WriteLine("command not known. Please use 'help' for help.");
-                    }
-                    break;
-
+                        break;
+                }
             }
         }
+
         public void help(String[] args)
         {
             if (args.Length > 1)
             {
-                Console.WriteLine("need help with: "+args[1]);
+                Console.WriteLine("need help with: " + args[1]);
             }
             else
             {
@@ -77,12 +123,12 @@ namespace flipflOS
         }
         public void writeToMemory(String[] args)
         {
-            if(args.Length <= 2)
+            if (args.Length <= 2)
             {
                 Console.WriteLine("Not enough Arguments. Syntax: write index data");
                 return;
             }
-            uint index = Convert.ToUInt32(args[1])*2;
+            uint index = Convert.ToUInt32(args[1]) * 2;
             byte data = Convert.ToByte(args[2]);
             mem.writeAt(index, data);
         }
